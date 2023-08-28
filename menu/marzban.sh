b16ef3f02c78d4a3c9a4df26ba4255bb1082eaf7
heruhendri <74039818+heruhendri@users.noreply.github.com>
2023-08-28 21:57:40 +0700

    Update marzban.sh

--- menu/marzban.sh
+++ menu/marzban.sh
@@ -16,7 +16,7 @@ echo -e " $COLOR1 $NC   ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH} INSTALL 
 echo -e " $COLOR1 $NC   ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH} INSTALL MARZBAN ${NC}                              $COLOR1 $NC"
 echo -e " $COLOR1 $NC   ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH} SETTING ${NC}                               $COLOR1 $NC"
 echo -e " $COLOR1 $NC                                               $COLOR1 $NC"
-echo -e " $COLOR1 $NC   ${WH}[${COLOR1}00${WH}]${NC} ${COLOR1}• ${WH}GO BACK${NC}                              $COLOR1 $NC"
+echo -e " $COLOR1 $NC   ${WH}[${COLOR1}0${WH}]${NC} ${COLOR1}• ${WH}GO BACK${NC}                              $COLOR1 $NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}"
 echo -e "$COLOR1 ${NC}                 ${WH}• hendr.store •${NC}                 $COLOR1 $NC"
@@ -27,6 +27,6 @@ case $opt in
 01 | 1) clear ; apt update && apt install wget -y && wget https://raw.githubusercontent.com/rizkyckj/xolpanel/master/xolpanel.sh && chmod +x xolpanel.sh && ./xolpanel.sh ;;
 02 | 2) clear ; wget https://raw.githubusercontent.com/Agunxzzz/MarXray/main/sslmar.sh && chmod 755 sslmar.sh && ./sslmar.sh ;;
 03 | 3) clear ; m-system ;;
-00 | 0) clear ; menu ;;
+0 | 0) clear ; menu ;;
 *) clear ; menu-backup ;;
 esac
