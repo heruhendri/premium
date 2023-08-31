89bd62bbf832ac9526545d45b894fc0001586441
heruhendri <74039818+heruhendri@users.noreply.github.com>
2023-08-31 14:25:28 +0700

    Update renew-ws.sh

--- xray/renew-ws.sh
+++ xray/renew-ws.sh
@@ -1,63 +1,62 @@
-clear
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             • RENEW VMESS USER •              ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
-if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-echo -e "$COLOR1│${NC}  • You have no existing clients!"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo -e "$COLOR1┌────────────────────── WM ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • Premium Auto Script •            $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo ""
-read -n 1 -s -r -p "   Press any key to back on menu"
-m-vmess
-fi
-clear
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             • RENEW VMESS USER •              ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq | nl
-echo -e "$COLOR1│${NC}"
-echo -e "$COLOR1│${NC}  • [NOTE] Press any key to back on menu"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo -e "$COLOR1┌────────────────────── WM ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • Premium Auto Script •            $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo -e "$COLOR1───────────────────────────────────────────────────${NC}"
-read -rp "   Input Username : " user
-if [ -z $user ]; then
-m-vmess
-else
-read -p "   Expired (days): " masaaktif
-if [ -z $masaaktif ]; then
-masaaktif="1"
-fi
-exp=$(grep -E "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-now=$(date +%Y-%m-%d)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-exp3=$(($exp2 + $masaaktif))
-exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "/### $user/c\### $user $exp4" /etc/xray/config.json
-systemctl restart xray > /dev/null 2>&1
-clear
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             • RENEW VMESS USER •              ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}   [INFO]  $user Account Renewed Successfully"
-echo -e "$COLOR1│${NC}   "
-echo -e "$COLOR1│${NC}   Client Name : $user"
-echo -e "$COLOR1│${NC}   Days Added  : $masaaktif Days"
-echo -e "$COLOR1│${NC}   Expired On  : $exp4"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo -e "$COLOR1┌────────────────────── WM ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • Premium Auto Script •            $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo ""
-read -n 1 -s -r -p "   Press any key to back on menu"
-m-vmess 
+#!/bin/bash  
+  # Color  
+  RED='\033[0;31m'  
+  NC='\033[0m'  
+  GREEN='\033[0;32m'  
+  ORANGE='\033[0;33m'  
+  BLUE='\033[0;34m'  
+  PURPLE='\033[0;35m'  
+  CYAN='\033[0;36m'  
+  LIGHT='\033[0;37m'  
+  # ==========================================  
+  # Getting  
+  dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')  
+  biji=`date +"%Y-%m-%d" -d "$dateFromServer"`  
+  #########################  
+  
+  MYIP=$(curl -sS ipv4.icanhazip.com)  
+  clear  
+  NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")  
+          if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then  
+                  clear  
+                  echo ""  
+                  echo "You have no existing clients!"  
+                  exit 1  
+          fi  
+  
+          clear  
+          echo ""  
+          echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"  
+          echo -e "$COLOR1│${NC} ${COLBG1}           • RENEW VMESS USER •              ${NC} $COLOR1│$NC"  
+          echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"  
+          echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"  
+  echo "     NO      USER          EXP" 
+          grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq | nl 
+          until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do  
+                  if [[ ${CLIENT_NUMBER} == '1' ]]; then  
+                          read -rp "Select one client [1]: " CLIENT_NUMBER  
+                  else  
+                          read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER  
+                  fi  
+          done  
+  read -p "Expired (Days): " masaaktif  
+  user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)  
+  exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)  
+  now=$(date +%Y-%m-%d)  
+  d1=$(date -d "$exp" +%s)  
+  d2=$(date -d "$now" +%s)  
+  exp2=$(( (d1 - d2) / 86400 ))  
+  exp3=$(($exp2 + $masaaktif))  
+  exp4=`date -d "$exp3 days" +"%Y-%m-%d"`  
+  sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json  
+  sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json  
+  systemctl restart xray.service  
+  service cron restart  
+  clear  
+  echo ""  
+  echo "==============================="  
+  echo "  Xray/Vmess Account Renewed  "  
+  echo "==============================="  
+  echo "Username  : $user"  
+  echo "Expired   : $exp4"  
+  echo "==============================="
