8ae834fcbc453d0af3bb8b5316ac0589c48f877f
heruhendri <74039818+heruhendri@users.noreply.github.com>
2023-08-31 12:21:25 +0700

    Update renew-tr.sh

--- xray/renew-tr.sh
+++ xray/renew-tr.sh
@@ -1,55 +1,64 @@
-#!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-echo "Checking VPS"
-clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		clear
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "\E[44;1;39m          ⇱ Renew Trojan ⇲         \E[0m"
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		echo ""
-		echo "You have no existing clients!"
-		echo ""
-		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo ""
-        read -n 1 -s -r -p "Press any key to back on menu"
-        m-trojan
-	fi
-
-	clear
-	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo -e "\E[44;1;39m          ⇱ Renew Trojan ⇲         \E[0m"
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo ""
-  	grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-    echo ""
-    red "tap enter to go back"
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	read -rp "Input Username : " user
-    if [ -z $user ]; then
-    m-trojan
-    else
-    read -p "Expired (days): " masaaktif
-    exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    now=$(date +%Y-%m-%d)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    exp3=$(($exp2 + $masaaktif))
-    exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-    sed -i "/#! $user/c\#! $user $exp4" /etc/xray/config.json
-    systemctl restart xray > /dev/null 2>&1
-    clear
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " Trojan Account Was Successfully Renewed"
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo ""
-    echo " Client Name : $user"
-    echo " Expired On  : $exp4"
-    echo ""
-    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo ""
-    read -n 1 -s -r -p "Press any key to back on menu"
-    m-trojan
-    fi
+ clear 
+ echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}" 
+ echo -e "$COLOR1 ${NC} ${COLBG1}            ${WH}• RENEW TROJAN USER •              ${NC} $COLOR1 $NC" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+ echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}" 
+ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json") 
+ if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then 
+ echo -e "$COLOR1 ${NC}  • You have no existing clients!" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"  
+ echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}" 
+ echo -e "$COLOR1 ${NC}                 ${WH}• PREMIUM •${NC}                 $COLOR1 $NC" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+ echo "" 
+ read -n 1 -s -r -p "   Press any key to back on menu" 
+ menu
+ fi 
+ clear 
+ echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}" 
+ echo -e "$COLOR1 ${NC} ${COLBG1}            ${WH}• RENEW TROJAN USER •              ${NC} $COLOR1 $NC" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+ echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}" 
+ grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq | nl 
+ echo -e "$COLOR1 ${NC}" 
+ echo -e "$COLOR1 ${NC}  ${COLOR1}• ${WH}[${COLOR1}NOTE${WH}] Press any key to back on menu" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"  
+ echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}" 
+ echo -e "$COLOR1 ${NC}                 ${WH}• PREMIUM •${NC}                 $COLOR1 $NC" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+ echo -e "$COLOR1───────────────────────────────────────────────────${NC}" 
+ read -rp "   Input Username : " user 
+ if [ -z $user ]; then 
+ menu 
+ else 
+ read -p "   Expired (days): " masaaktif 
+ if [ -z $masaaktif ]; then 
+ masaaktif="1" 
+ fi 
+ exp=$(grep -E "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq) 
+ now=$(date +%Y-%m-%d) 
+ d1=$(date -d "$exp" +%s) 
+ d2=$(date -d "$now" +%s) 
+ exp2=$(( (d1 - d2) / 86400 )) 
+ exp3=$(($exp2 + $masaaktif)) 
+ exp4=`date -d "$exp3 days" +"%Y-%m-%d"` 
+ sed -i "/#! $user/c\#! $user $exp4" /etc/xray/config.json 
+ systemctl restart xray > /dev/null 2>&1 
+ clear 
+ echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}" 
+ echo -e "$COLOR1 ${NC} ${COLBG1}            ${WH}• RENEW TROJAN USER •              ${NC} $COLOR1 $NC" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+ echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}" 
+ echo -e "$COLOR1 ${NC}   [INFO]  $user Account Renewed Successfully" 
+ echo -e "$COLOR1 ${NC}   " 
+ echo -e "$COLOR1 ${NC}   Client Name : $user" 
+ echo -e "$COLOR1 ${NC}   Days Added  : $masaaktif Days" 
+ echo -e "$COLOR1 ${NC}   Expired On  : $exp4" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"  
+ echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}" 
+ echo -e "$COLOR1 ${NC}                 ${WH}• PREMIUM •${NC}                 $COLOR1 $NC" 
+ echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+ echo "" 
+ read -n 1 -s -r -p "   Press any key to back on menu" 
+ menu
+ fi
