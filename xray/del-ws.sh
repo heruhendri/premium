c0b4556d1c9733c14d43b7c74663d5f822c4d307
heruhendri <74039818+heruhendri@users.noreply.github.com>
2023-09-05 00:31:35 +0700

    Update del-ws.sh

--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -23,7 +23,8 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 	read -rp "   Input Username : " user
  	if [ -z $user ]; then
-	done
+  	m-vmess
+	else
 user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "$user"p)
 exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "$user"p)
 sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
